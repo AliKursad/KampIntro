582749fc64f5140d1b132bac4eec63caecedb785
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2021-01-16 23:48:35 +0300

    16.01.2021

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string KategoriEtiketi = "Kategori";
+            int OgrenciSayisi = 32000;
+            double FaizOrani = 1.45;
+            bool SistemeGirisYapmisMi = true;
+            double DolarDun = 7.35;
+            double DolarBugun = 7.45;
+
+            if (DolarDun>DolarBugun)
+            {
+                Console.WriteLine("Azalan Ok");
+            }
+            else if (DolarDun<DolarBugun)
+            {
+                Console.WriteLine("Artan ok");
+            }
+            else
+            {
+                Console.WriteLine("eşittir");
+            }
+           
+
+            if (SistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+
+            Console.WriteLine(KategoriEtiketi);
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(15, 26);
+
+            dortIslem.Topla(825, 6985);
+
+        }
+    }
+}
